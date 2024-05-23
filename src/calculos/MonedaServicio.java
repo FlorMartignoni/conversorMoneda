1ed3ba9167fba9b5144f4b28893ff4194e84bec1
Flor Martignoni <flor.martignoni@gmail.com>
2024-05-23 20:09:38 -0300

    Primera estructura

---
+++ src/calculos/MonedaServicio.java
@@ -0,0 +1,6 @@
+package calculos;
+
+public class MonedaServicio {
+
+
+}
---
+++ src/modelos/Moneda.java
@@ -0,0 +1,8 @@
+package modelos;
+
+public class Moneda {
+
+    private String monedaBase;
+    private Double valor;
+
+}
